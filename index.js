ba70f20f9916257d39415bc48c56dbff7dc74fd8
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-24 00:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function add2(n) {
 var funkyFunction = function() {
   return function(){
     return "FUNKY!";
-  }
-}
+  };
+};
 
-var theFunk = funkyFunction();
+var theFunk = funkyFunction()();
 theFunk();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
