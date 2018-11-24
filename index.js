24fab0bc8619ba201bcec5eea7ef5aa39d33382e
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-24 00:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ var funkyFunction = function() {
     return "FUNKY!";
 };
 };
-var theFunk = funkyFunction()();
+var theFunk = funkyFunction();
+theFunk();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
